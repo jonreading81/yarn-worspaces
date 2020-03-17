cd270e3e53dd617ba6e6cf78c023f823d4438b21
Jonathan Reading <jon.reading.email@gmail.com>
2020-03-17 14:34:59 +0000

    Fix typos

--- scripts/affected.js
+++ scripts/affected.js
@@ -4,26 +4,26 @@ const { exec } = require('child_process');
 const gitStartHash = process.argv[2];
 const gitEndHash = process.argv[3];
 const command = 'yarn workspaces info --json';
-                 
+
 const parseWorkspaces = (str) => {
-    const {data}  = JSON.parse(str);
-    const objWorkspaces = JSON.parse( data);
-    return Object.keys(objWorkspaces).map(name => ({name, ...objWorkspaces[name]}));
+    const { data } = JSON.parse(str);
+    const objWorkspaces = JSON.parse(data);
+    return Object.keys(objWorkspaces).map(name => ({ name, ...objWorkspaces[name] }));
 }
 
-const isWorkpaceUpdated =  ({location}) => folderContentsChanged(`${location}/**/*.js`, gitStartHash, gitEndHash);
+const isworkspaceUpdated = ({ location }) => folderContentsChanged(`${location}/**/*.js`, gitStartHash, gitEndHash);
 
-const areWorkspaceDependenciesUpdated = ({workspaceDependencies}, updatedWorspacesNames) =>  
-    workspaceDependencies.some(workspace =>updatedWorspacesNames.includes(workspace));
-const isWorkspaceAffected =({workspaceDependencies, name}, updatedWorspacesNames) => 
-!updatedWorspacesNames.includes(name) 
-&& areWorkspaceDependenciesUpdated({workspaceDependencies, name}, updatedWorspacesNames)
+const areWorkspaceDependenciesUpdated = ({ workspaceDependencies }, updatedWorkspacesNames) =>
+    workspaceDependencies.some(workspace => updatedWorkspacesNames.includes(workspace));
+const isWorkspaceAffected = ({ workspaceDependencies, name }, updatedWorkspacesNames) =>
+    !updatedWorkspacesNames.includes(name)
+    && areWorkspaceDependenciesUpdated({ workspaceDependencies, name }, updatedWorkspacesNames)
 
 exec(command, (error, stdout, stderr) => {
-const arrWorkspaces = parseWorkspaces(stdout)
-const updatedWorspaces = arrWorkspaces.filter(isWorkpaceUpdated);
-const updatedWorspacesNames = updatedWorspaces.map(({name}) => name);
-const affectedWorkspaces=arrWorkspaces.filter(workpace=> isWorkspaceAffected(workpace, updatedWorspacesNames));
-const  allAffected = [...updatedWorspaces, ...affectedWorkspaces].map(({name}) => name);
-console.log(allAffected);
+    const arrWorkspaces = parseWorkspaces(stdout)
+    const updatedWorkspaces = arrWorkspaces.filter(isworkspaceUpdated);
+    const updatedWorkspacesNames = updatedWorkspaces.map(({ name }) => name);
+    const affectedWorkspaces = arrWorkspaces.filter(workspace => isWorkspaceAffected(workspace, updatedWorkspacesNames));
+    const allAffected = [...updatedWorkspaces, ...affectedWorkspaces].map(({ name }) => name);
+    console.log(allAffected);
 });
